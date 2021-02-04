6f1260858e4468ad0d52944c9d218362010e259a
Artur Grudkowski <agrudkow@gmail.com>
2021-02-04 21:18:58 +0100

    refactor(zad_1.py): prettify, refactor and document

--- zad_1.py
+++ zad_1.py
@@ -2,6 +2,10 @@ from typing import List, TextIO
 
 
 def convert_char_to_decimal(char: str) -> int:
+    '''
+    Funkcja konvertująca znaki do reprezentacji decymalnej
+    wskazanej w treści zadania.
+    '''
     ascii_repr = ord(char)
 
     if ascii_repr >= ord('0') and ascii_repr <= ord('9'):
@@ -15,48 +19,69 @@ def convert_char_to_decimal(char: str) -> int:
         raise RuntimeError(msg)
 
 
-def converter(number: str, base: int) -> int:
-    # split number in figures
+def converter_base_to_decimal(number: str, base: int) -> int:
+    '''
+    Funkcja konwertująca liczbę w reprezentacji o podstawie
+    `base` do reprezentacji dziesiętnej
+    '''
+    # Rozdziel liczbę na pszczególne znaki (liczby)
+    # Konwertuj znaki do postaci decymalnej
     figures = [convert_char_to_decimal(i) for i in number]
-    # invert oder of figures (lowest count first)
+    # Odwróć kolejność liczb
     figures = figures[::-1]
+
     result: int = 0
-    # loop over all figures
+    # Iteruj po liczbach i agregują sumę po konwersji
     for i in range(len(figures)):
-        # add the contirbution of the i-th figure
         result += figures[i] * base**i
     return result
 
 
-def decode_nunmbers(base: int, number: str) -> int:
-    return converter(number, base)
+def decode_number(encoded_number: str) -> int:
+    '''
+        Funkcja dekodująca pojedynczą zakodowaną liczbę.
+
+        Funkcja rozdziela liczbę od jej bazy i wywołuje
+        na niej konwerter.
+    '''
+    base, number = encoded_number.split(':')
+    converted_base = convert_char_to_decimal(base) + 1
+    return converter_base_to_decimal(number, converted_base)
 
 
-def extract_encoded_numbers(line: str) -> List[int]:
-    extracted_encoded_numbers: List[int] = []
-
-    for encoded_tuple in line.split(';'):
-        base, number = encoded_tuple.split(':')
-        converted_base = convert_char_to_decimal(base) + 1
-        extracted_encoded_numbers.append(
-            decode_nunmbers(converted_base, number))
-
-    return extracted_encoded_numbers
-
-
-def parse_to_decimal(line: str) -> List[int]:
-    extracted_encodings = extract_encoded_numbers(line)
-    # print(extracted_encodings)
-    return extracted_encodings
+def decode_numbers(encoded_numbers: List[str]) -> List[int]:
+    '''
+    Funkcja iterująca po liście zakodowanych liczb
+    i wywołująca na nich funkcję dekodującą. Funkcja
+    ta zwraca zdekodowaną listę liczb.
+    '''
+    return [
+        decode_number(encoded_number) for encoded_number in encoded_numbers
+    ]
 
 
 def map_file(file: TextIO) -> List[List[int]]:
+    '''
+    Funkcja iterująca po kolejnych liniach w pliku `file`
+    i zwracająca dla każdej lini listę zdekodowanych
+    liczb do reprezentacji dziesiętnej.
+
+    Każda koljna linia jest pozbawiana początkowych i końcowych
+    znaków białych oraz końcowego średnika, a do funkjcji dekodującej
+    przekazywane są wekstrahowane, zakodowane liczby.
+    '''
     parsed_list: List[List[int]] = []
     for line in file:
-        parsed_list.append(parse_to_decimal(line.strip().rstrip(';')))
+        line = line.strip().rstrip(';')
+        parsed_list.append(decode_numbers(line.split(';')))
     return parsed_list
 
 
 if __name__ == '__main__':
+    results: List[List[int]] = []
+
     with open('data_1') as file:
-        print('numbers', map_file(file))
+        results = map_file(file)
+
+    for line in results:
+        print(line)
